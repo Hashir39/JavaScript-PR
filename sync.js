b2e095833f83a8ee8ad2ffcba146bbaf131309b8
Muhammad Hashir <hashirshakeel044@gmail.com>
2025-06-17 20:57:22 +0500

    2

---
+++ sync.js
@@ -0,0 +1,7 @@
+//Asyncronous - sab sath print hoga like timeout
+//Synchronous - ek ek krky chlty like 1 prompt then 2nd then 3rd
+console.log("start")
+setTimeout(() => {
+    console.log("hey")
+}, 3000);
+console.log("End")
