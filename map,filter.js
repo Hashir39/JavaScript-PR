04a1eff5e4adcd206a314cb09a44e31477c257f1
Hashir39 <126954940+Hashir39@users.noreply.github.com>
2025-06-19 21:10:01 +0500

    Update map,filter.js

--- map,filter.js
+++ map,filter.js
@@ -1,18 +1,18 @@
  //map (creates a new array)
  //foreach (updates exisiting array)
-let arr = [34,23,35]
-const a = arr.map((value,index,array)=>{
+let arrr = [34,23,35]
+const a = arrr.map((value,index,array)=>{
     console.log(value,index,array)
     console.log(value+index)
 })
 
-const b = arr.filter((a)=>{
+const b = arrr.filter((a)=>{
         console.log(a>25)
         return a>25
 })
 console.log(b)
 
-const c = arr.reduce((h1,h2)=>{
+const c = arrr.reduce((h1,h2)=>{
     return h1 + h2 
 }) //reduce takes first 2 values and adds then takes next value then add
 console.log(c)
