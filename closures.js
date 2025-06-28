516338afd1688d80b4c686018da1cd7ca4ec0f16
Muhammad Hashir <hashirshakeel044@gmail.com>
2025-06-28 18:51:14 +0500

    222

---
+++ closures.js
@@ -0,0 +1,71 @@
+function outer() {
+  let counter = 0;
+
+  return function inner() {
+    counter++;
+    console.log(counter);
+  };
+}
+
+const fn = outer();
+fn(); // 1
+fn(); // 2
+fn(); // 3
+
+
+//1. Private Variables
+function createCounter() {
+  let count = 0;
+
+  return {
+    increment: function () {
+      count++;
+      return count;
+    },
+    decrement: function () {
+      count--;
+      return count;
+    }
+  };
+}
+
+const counter = createCounter();
+console.log(counter.increment()); // 1
+console.log(counter.increment()); // 2
+console.log(counter.decrement()); // 1
+
+
+
+//2. setTimeout in a Loop Fix
+for (var i = 1; i <= 3; i++) {
+  (function (j) {
+    setTimeout(() => console.log(j), 1000 * j);
+  })(i);
+}
+// Output: 1 2 3 (1s apart)
+
+
+
+//Interview-Style Closure Question
+function multiplier(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+
+const double = multiplier(2);
+console.log(double(5)); // 10
+
+
+
+
+
+function secretPassword() {
+  const password = "abc123";
+  return function reveal() {
+    return password;
+  };
+}
+
+const getPassword = secretPassword();
+console.log(getPassword()); // "abc123"
