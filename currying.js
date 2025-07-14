810932857f20b41a31d22e2cb5261d3cdaa6ce72
Hashir39 <126954940+Hashir39@users.noreply.github.com>
2025-07-14 14:45:09 +0500

    Update currying.js

--- currying.js
+++ currying.js
@@ -21,3 +21,25 @@ const curriedAdd = a => b => c => a + b + c;
 
 console.log(curriedAdd(1)(2)(3)); // 6
 
+
+
+//Real-World Example
+function filterBy(type) {
+  return function(value) {
+    return function(item) {
+      return item[type] === value;
+    };
+  };
+}
+
+const filterByCategory = filterBy('category')('books');
+
+const items = [
+  { name: 'JS Book', category: 'books' },
+  { name: 'CSS Shirt', category: 'clothes' }
+];
+
+const result = items.filter(filterByCategory);
+console.log(result); // [{ name: 'JS Book', category: 'books' }]
+
+
