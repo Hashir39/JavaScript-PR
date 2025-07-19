4640bac97dbc6bdd573769bdc4e43f7696599691
Hashir39 <126954940+Hashir39@users.noreply.github.com>
2025-07-19 18:51:19 +0500

    Update asyncawait.js

--- asyncawait.js
+++ asyncawait.js
@@ -11,7 +11,7 @@ async function weeater(){
     })
 
     console.log("Fetching weather 1")
-    let weath1 = await weeather1
+    let weath1 = await weeather
     console.log(weath1)
     console.log("Fetching weeather 2")
     let weath2 = await weeather2
